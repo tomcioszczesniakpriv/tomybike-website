889a8a27cc6ef75cd6e4a690e6907086f8539b9a
tomcioszczesniakpriv <tomcioszczesniakpriv@gmail.com>
2026-07-28 20:04:00 +0100

    Add files via upload

--- products.js
+++ products.js
@@ -15,16 +15,16 @@ window.PRODUCTS = [
     "status": "Available",
     "badge": "Good Price",
     "images": [
-      "assets/trek-domane-al3-01.jpeg",
-      "assets/trek-domane-al3-02.jpeg",
-      "assets/trek-domane-al3-03.jpeg",
-      "assets/trek-domane-al3-04.jpeg",
-      "assets/trek-domane-al3-05.jpeg",
-      "assets/trek-domane-al3-06.jpeg",
-      "assets/trek-domane-al3-07.jpeg",
-      "assets/trek-domane-al3-08.jpeg",
-      "assets/trek-domane-al3-09.jpeg",
-      "assets/trek-domane-al3-10.jpeg"
+      "assets/43E60FD0-9B26-4C9F-B53D-8E1170935943.jpeg",
+      "assets/35A9BA03-30FC-49DA-8533-6C1F5B979006.jpeg",
+      "assets/DF04D5AB-A376-445E-80F1-C78D92C8C85E.jpeg",
+      "assets/6F280529-9456-4649-B91A-2BC442CCEBF0.jpeg",
+      "assets/FD6CFA24-ACA1-4CD3-8339-4CDBB3C26653.jpeg",
+      "assets/732D9A2C-251D-4B6B-97AE-5FC8D6FACEBF.jpeg",
+      "assets/7FAF0426-B29A-43C4-8031-2318C144E4A8.jpeg",
+      "assets/433820BA-17C6-45BA-8AC6-4BDAD792D19D.jpeg",
+      "assets/4000CF38-F186-4919-B5AC-524D0F5FF246.jpeg",
+      "assets/738877B0-1901-4EA6-B308-B745BB812F84.jpeg"
     ],
     "features": [
       "Endurance road bike",
